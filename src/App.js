ec8bea0810ed151bf67ebc8c46f24ab720cc4cf5
root <root@localhost.localdomain>
2019-12-16 10:45:00 -0500

    first_commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function main () {
+  return (
+    <div className="Menu">
+      <header className="Nav_bar">
+        <p>Main</p>
+        <p>Sign-In</p>
+        <p>About</p>
+        <p>Contact</p>
+      </header>
+    </div>
+  )
+};
+export default main;
+
+
+// function App1() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App1;
